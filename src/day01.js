dcc47821915eb7d56176fc157a5d4b38caf37fe7
GA1 <nvkga1@gmail.com>
2017-12-12 01:30:16 +0100

    Finish day01

--- src/day01.js
+++ src/day01.js
@@ -1,6 +1,14 @@
 function captcha(s) {
-  var chars = s.split()
-  // for   ()
-  return 2
+  var chars = s.split('')
+  var counter = 0
+  let N = chars.length;
+  for (let i = 1; i < N; i++)
+    if (chars[i - 1] === chars[i])
+      counter += Number(chars[i])
+  if (s.length > 1)
+    if (chars[0] === chars[N - 1])
+      counter += Number(chars[N - 1])
+  return counter
 }
+
 module.exports = captcha;
